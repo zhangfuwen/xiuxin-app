630664ad173fd4e6872fd947b941323fc07eaa22
zhangfuwen <zhangfuwen@users.noreply.github.com>
2026-03-15 14:14:36 +0800

    fix: Add missing ViewGroup import in BreathingFragment

--- android/app/src/main/java/com/xiuxin/app/fragment/BreathingFragment.java
+++ android/app/src/main/java/com/xiuxin/app/fragment/BreathingFragment.java
@@ -7,6 +7,7 @@ import android.os.Handler;
 import android.os.Vibrator;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
 import android.view.animation.ScaleAnimation;
